ebb2d5547411f67366a9599380536483bd864fad
MinBin <lin_minbin@hotmail.com>
2018-05-25 13:15:18 +0200

    update “data collection for route optimisation” and relevant files

--- Min-Bin Lin/DataCollection_route optimisation.py
+++ Min-Bin Lin/DataCollection_route optimisation.py
@@ -23,7 +23,7 @@ from scipy.spatial.distance import squareform
 # google map api (2,500 free requests per day): geocoding and distance matrix
 # api key: https://developers.google.com/maps/documentation/geocoding/get-api-key
 import googlemaps
-gmaps = googlemaps.Client(key = 'AIzaSyBStGhUd8RwlGYmU7fFaQN9tjMmrycG7ls')
+gmaps = googlemaps.Client(key = 'ENTER YOUR KEY HERE')
 
 logger = logging.getLogger("DataCollection")
 logger.basicConfig = logging.basicConfig(level=logging.DEBUG)
